d08439ab9670f87e3b087646aa5e8bdc8e2f1117
Jabbar <multimediary@gmail.com>
2021-05-23 21:55:38 +0700

    Hide Key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: '5a393b3e568f5df5467c5b8e5bddb234',
+  KEY: '',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
